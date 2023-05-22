3d94f778d43110302d4c84e145ae3823b3ac33d8
Leonce Nshuti <lnshuti@Leonces-Mac-mini.local>
2023-05-21 19:54:47 -0500

    Add streamlit app

---
+++ chat_my_data.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+def main():
+
+     st.set_page_config(layout="wide")
+     st.title('Chat with Docs')
+
+     st.write('''User-friendly web app that enables users to search for specific information from various sources, including websites, PDF documents, and text files, while maintaining a contextual conversation with an AI-powered chatbot, with admin capabilities for easy management and customization.''')
+
+main()
